93506a0ab2d5182a3a82cc444c27a85eb2ed66ce
vutiendat3601 <vutien.dat.3601@gmail.com>
2022-12-11 20:22:18 +0700

    init

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,26 +108,6 @@ const app = {
         timerCurrent.textContent = secondsToMinutesAndSeconds(0);
         timerEnd.textContent = secondsToMinutesAndSeconds(audio.duration);
     },
-    uploadSong: async function (mp3File) {
-        let buffer = {};
-        await mp3File.arrayBuffer().then((data) => buffer = data);
-        const content = bufferToBase64(buffer);
-        let url = `https://api.github.com/repos/vutiendat3601/music-app/contents/assets/audio/${mp3File.name}`;
-        github.update(url, { content: content, message: `add ${mp3File.name}` });
-    },
-    updateSettings: function () {
-        console.log(this.settings);
-        let content = bufferToBase64(this.settings);
-        // console.log(content);
-        github.update(settingsURL, {
-            message: "update settings",
-            content: content,
-            sha: this.settings.sha
-        });
-    },
-    updateSongs: function () {
-
-    },
     initHandler: function () {
         let _this = this;
 
@@ -284,22 +264,7 @@ const app = {
             e.stopPropagation();
 
             let mp3File = inpMp3.files[0];
-            this.uploadSong(mp3File);
-
-            this.settings.numOfSong++;
-            const newSong = {
-                id: this.settings.numOfSong,
-                name: inpSongName.value,
-                singer: inpSinger.value,
-                youtube: inpYoutube.value,
-                thumb: "",
-                path: "hai-muoi-hai_amee.mp3"
-            }
-            this.songs.push(newSong);
-            this.updateSettings();
-            // this.updateSongs();
-            this.settings
-
+            alert(mp3File.name);
             modalOverlay.click();
         }
         modalOverlay.onclick = () => {
@@ -343,54 +308,4 @@ function toNonAccentVietnamese(str) {
     str = str.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, ""); // Huyền sắc hỏi ngã nặng 
     str = str.replace(/\u02C6|\u0306|\u031B/g, ""); // Â, Ê, Ă, Ơ, Ư
     return str;
-}
-
-const github = {
-    committer: {
-        name: "vutiendat3601",
-        email: "vutien.dat.3601@gmail.com"
-    },
-    prepareHeader: function (url) {
-        let requestHeaders = new Headers();
-        requestHeaders.append("Authorization", "Bearer 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");
-        requestHeaders.append("Content-Type", "application/json");
-        return requestHeaders;
-    },
-    fetch: async function (url) {
-        let requestHeaders = this.prepareHeader();
-        requestOptions = {
-            method: 'GET',
-            headers: requestHeaders,
-            redirect: 'follow'
-        };
-        const result = await fetch(url, requestOptions).then((data) => data.json());
-        return result.json();
-    },
-    update: function (url, json) {
-        let requestHeaders = this.prepareHeader();
-        raw = {
-            ...this.committer,
-            ...json,
-        }
-        let body = JSON.stringify(raw);
-        let requestOptions = {
-            method: 'put',
-            headers: requestHeaders,
-            body: body,
-            redirect: 'follow'
-        };
-        console.log(requestOptions);
-        fetch(url, requestOptions).then((data) => data.text()).then((text) => console.log(text))
-            .catch((error) => console.log("error: ", error));
-    }
-}
-
-let bufferToBase64 = function (buffer) {
-    var bytes = new Uint8Array(buffer);
-    var len = buffer.byteLength;
-    var binary = "";
-    for (var i = 0; i < len; i++) {
-        binary += String.fromCharCode(bytes[i]);
-    }
-    return window.btoa(binary);
 }
